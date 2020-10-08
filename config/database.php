02130f1e903c46c4ef571cedf56f052ce666fb3a
sebastienvial <sebastien.vial68@gmail.com>
2020-10-08 17:54:23 +0000

    add database pg

--- config/database.php
+++ config/database.php
@@ -91,6 +91,17 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'heroku' => [
+'driver' => 'pgsql',
+'host' => parse_url(getenv("DATABASE_URL"))["host"],
+'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+'username' => parse_url(getenv("DATABASE_URL"))["user"],
+'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+'charset' => 'utf8',
+'prefix' => '',
+'schema' => 'public',
+]
+
     ],
 
     /*
